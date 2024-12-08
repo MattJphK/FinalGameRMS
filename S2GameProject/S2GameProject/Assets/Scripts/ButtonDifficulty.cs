c334f6d4bf3334936205cff5cda45bd94359fd66
MattJphK <b00160049@mytudublin.ie>
2024-12-08 21:46:23 +0000

    working on difficulty

--- S2GameProject/S2GameProject/Assets/Scripts/ButtonDifficulty.cs
+++ S2GameProject/S2GameProject/Assets/Scripts/ButtonDifficulty.cs
@@ -7,14 +7,26 @@ public class ButtonDifficulty : MonoBehaviour
 {
     private Button button;
     private UIManger uIManger;
+    private Enemy enemyScript;
     public int difficulty;
 
     // Start is called before the first frame update
     void Start()
     {
         uIManger = GameObject.Find("UI Manager").GetComponent<UIManger>();
+        enemyScript = GameObject.Find("Enemy").GetComponent<Enemy>();
         button = GetComponent<Button>();
         button.onClick.AddListener(SetDifficulty);
+        if(difficulty == 0){
+            Enemy.speed = 40.0f;
+
+        }
+        else if(difficulty == 1){
+            Enemy.speed = Enemy.speed * 2;
+        }
+        else if(difficulty == 2){
+            Enemy.speed = Enemy.speed * 3;
+        }
     }
 
     // Update is called once per frame
