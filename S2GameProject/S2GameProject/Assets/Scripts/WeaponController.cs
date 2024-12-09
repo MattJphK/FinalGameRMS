bb1524c6ec923eaea92cdf592c9e5547905faaef
B00166345 <b00166345@mytudublin.ie>
2024-12-09 20:00:49 +0000

    BulletLimit

--- S2GameProject/S2GameProject/Assets/Scripts/WeaponController.cs
+++ S2GameProject/S2GameProject/Assets/Scripts/WeaponController.cs
@@ -5,33 +5,42 @@ using UnityEngine;
 public class WeaponController : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    public float bulletLimit = 5.0f;
+    public float bulletLimit = 3.0f;
     private UIManger uIManger;
     private bool canShoot = true;
-    // Start is called before the first frame update
+
     void Start()
     {
         uIManger = GameObject.Find("UI Manager").GetComponent<UIManger>();
     }
 
-    // Update is called once per frame
-void Update()
-{
-    if (uIManger.gameIsOn && canShoot)
+    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.X))
+        if (uIManger.gameIsOn && canShoot)
         {
-            Instantiate(bulletPrefab, transform.position, bulletPrefab.transform.rotation);
-            StartCoroutine(BulletCooldown());
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                // Count current bullets
+                int bulletCount = GameObject.FindGameObjectsWithTag("Bullet").Length;
+
+                // Check if under the limit
+                if (bulletCount < bulletLimit)
+                {
+                    Instantiate(bulletPrefab, transform.position, bulletPrefab.transform.rotation);
+                    StartCoroutine(BulletCooldown());
+                }
+                else
+                {
+                    Debug.Log("Bullet limit reached. Wait until a bullet is destroyed before firing again.");
+                }
+            }
         }
     }
-}
 
-IEnumerator BulletCooldown()
-{
-    canShoot = false;
-    yield return new WaitForSeconds(1.2f); // wait 5 seconds
-    canShoot = true;
-}
-    
+    IEnumerator BulletCooldown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(1.2f);
+        canShoot = true;
+    }
 }
